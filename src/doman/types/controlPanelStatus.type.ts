d7d550bbd564e5e07e53c133a005c89a4218b4ea
John <johnelvis963@gmail.com>
2025-05-02 19:31:05 -0500

    refactor: reorganize control panel interfaces and improve type definitions

--- src/doman/types/controlPanelStatus.type.ts
+++ src/doman/types/controlPanelStatus.type.ts
@@ -1,19 +1,25 @@
-import { MessageEditOptions, MessagePayload } from 'discord.js';
+import { ActionRow, ButtonComponent, Embed, InteractionResponse, Message } from 'discord.js';
 
+// components: ActionRow<ButtonComponent>[]; // Representa filas de botones en Discord
+// embeds: Embed[]; // Representa los embeds en Discord
+interface IControlPanel extends Message<true> {
+   components: any[]; // Representa filas de botones en Discord
+   embeds: Embed[]; // Representa los embeds en Discord
+}
+
+interface IControlPanel1 extends InteractionResponse<boolean> {
+   components: ActionRow<ButtonComponent>[]; // Representa filas de botones en Discord
+   embeds: Embed[]; // Representa los embeds en Discord
+}
 
 export interface IControlPanelStatus {
-   guildId: string,
-   channelId: string,
-   autorUserId?: string,
-   isPause: boolean,
-   isResume: boolean,
-   isMuteSong: boolean,
-   isActiveSong: boolean,
-   volumen: number,
-   controlPanel: {
-      delete: () => void;
-      edit: (content: string | MessageEditOptions | MessagePayload) => void;
-      components: any[]
-      embeds: any[]
-   }
+   guildId: string; // ID del servidor
+   channelId?: string; // ID del canal
+   autorUserId?: string; // ID del usuario que controla (opcional)
+   isPause?: boolean; // Indica si está pausado
+   isResume?: boolean; // Indica si está reanudado
+   isMuteSong?: boolean; // Indica si la canción está silenciada
+   isActiveSong?: boolean; // Indica si hay una canción activa
+   volumen?: number; // Volumen actual
+   controlPanel: IControlPanel | IControlPanel1; // Panel de control
 }
