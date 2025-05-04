bf90456410d7570b8bbb33f376904815a420849f
John <johnelvis963@gmail.com>
2025-05-03 23:54:33 -0500

    Refactor error handling in controller command to use custom errors and improve code clarity

--- src/interface/commands/controller.ts
+++ src/interface/commands/controller.ts
@@ -1,6 +1,8 @@
 import { SlashCommandBuilder } from 'discord.js';
+import { CustonError } from '../../doman/error';
 import { CustonInteraction } from '../../doman/types'
-import { PanelStatusComponent, EmdebComponent } from '../../infrastructure/discord';
+import { ErrorService } from '../../application/service';
+import { PanelStatusComponent } from '../../infrastructure/discord';
 import { PanelStatusHandler } from '../../application/handler/controlPanel';
 
 
@@ -10,7 +12,6 @@ const options = {
       .setDescription('Panel de control')
 }
 
-
 const execute = async (interaction: CustonInteraction) => {
    if (!interaction.isChatInputCommand()) return;
 
@@ -18,10 +19,7 @@ const execute = async (interaction: CustonInteraction) => {
       const queue = interaction?.client?.player?.getQueue(interaction.guildId!);
 
       if (!queue || !queue.playing || queue.songs.length === 0) {
-         return interaction.reply({
-            embeds: [EmdebComponent.emdebError('⛔ No hay música reproduciéndose.')],
-            ephemeral: true
-         })
+         throw CustonError.validation('⛔ No hay música reproduciéndose.')
       };
 
       const panelControlComponent = new PanelStatusComponent()
@@ -37,21 +35,24 @@ const execute = async (interaction: CustonInteraction) => {
             quantityInQueue: String(queue.songs.length),
             volumen: String(queue.volume),
          })
+         .footer({
+            text: `\`\``,
+            iconUser: 'https://res.cloudinary.com/df4jfvyjm/image/upload/v1746313476/qukbvlboemwfpgxgdd5s.gif'
+         })
          .build()
+
 
       const components = panelControlComponent.buttons.create({
          isActiveSong: queue?.volume > 0,
-         isMuteSong: queue?.volume === 0,
-         isPaused: queue?.paused,
          isPlaying: queue?.playing && !queue?.paused,
+         isActiveLoop: queue?.repeatMode === 2,
       }).buildRows()
 
-      const sentMessage = await interaction.reply({
+      await interaction.reply({
          embeds: [embed],
          components: components,
-         fetchReply: true,
       });
-
+      const sentMessage = await interaction.fetchReply();
       PanelStatusHandler.delete(interaction.client, queue.textChannel?.guildId!)
 
       PanelStatusHandler.create(interaction.client, {
@@ -60,9 +61,8 @@ const execute = async (interaction: CustonInteraction) => {
       })
 
    } catch (error) {
-
+      ErrorService.reply(interaction, error as Error)
    }
-
 }
 
 
