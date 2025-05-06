15a5326a3ccd38adee04134bc83292fa735e50c9
John <johnelvis963@gmail.com>
2025-05-05 22:59:35 -0500

    Archivo de configuracion

---
+++ src/config/foundComponent.ts
@@ -0,0 +1,19 @@
+
+export const ConfigFoundComponent = {
+   color: '#5865f2',
+   nameAutor: {
+      label: '',
+      maxLength: 25,
+      ellipsis: '...',
+      canShowIconUrl: true,
+   }, // agredo:
+
+   field: {
+      autor: '',
+      duration: '',
+      source: '',
+   },
+
+   nameAutorFormat: (key: string, value: string) => `${key}: **${value}**`,
+   fieldFormat: (key: string, value: string) => `• ${key}: **${value}**`,
+}
