d51b830d7a5b0a8497aa8c97abd58e474c9cd91c
John <johnelvis963@gmail.com>
2025-05-05 23:03:18 -0500

    Archivo de eventos

---
+++ src/doman/types/eventButtons.ts
@@ -0,0 +1,14 @@
+
+
+export const EventButtons = {
+   BTN_BACK: 'btnBack',
+   BTN_PAUSE: 'btnPause',
+   BTN_PLAY: 'btnPlay',
+   BTN_SKIP: 'btnSkip',
+   BTN_STOP: 'btnStop',
+   BTN_LOOPDISACTIVE: 'btnLoopDesactive',
+   BTN_LOOPACTIVE: 'btnLoopActive',
+   BTN_MUTESONG: 'btnMuteSong',
+   BTN_ACTIVESONG: 'btnActiveSong',
+   BTN_PLAYLIST: 'btnPlaylist',
+}
