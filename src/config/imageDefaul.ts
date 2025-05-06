fb9f7c04aebce2478215eea5d3c73c7bb4c869e7
John <johnelvis963@gmail.com>
2025-05-05 22:59:44 -0500

    imagen defaul

---
+++ src/config/imageDefaul.ts
@@ -0,0 +1,6 @@
+export const imageDefaul: { [key: string]: string } = {
+   soundcloud: 'https://res.cloudinary.com/df4jfvyjm/image/upload/v1746114336/uzyz3dttiftkpjqt2tuf.png',
+   youtube: 'https://res.cloudinary.com/df4jfvyjm/image/upload/v1746114335/lidqhayybfxkf5pbjfxg.png',
+   spotify: 'https://res.cloudinary.com/df4jfvyjm/image/upload/v1746114335/jsribordfkvsj4pjnyn1.png',
+   defaulImagenMusic: 'https://res.cloudinary.com/df4jfvyjm/image/upload/v1746389023/c2gw81pbqsimneruyyde.gif',
+}
