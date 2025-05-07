e9a26b53591d0a731108bfbb4188aa5c68ac9667
John <johnelvis963@gmail.com>
2025-05-06 19:42:03 -0500

    fix: Se hace uso en cada metodo staticos de PanelStatusHandler, para obtener el idioma actual del servidor

--- src/application/service/error.ts
+++ src/application/service/error.ts
@@ -1,56 +1,82 @@
 import { Queue } from 'distube';
+import { Timeout } from '../../utils';
 import { CustonError } from '../../doman/error';
 import { CustonInteraction } from '../../doman/types';
 import { EmbedComponent } from '../../infrastructure/discord';
-import { Timeout } from '../../utils';
+import { PanelStatusHandler } from '../handler/controlPanel';
 
 export class ErrorService {
 
-   static async reply(interaction: CustonInteraction, error: Error, time: number = 2_000): Promise<void> {
-
-      if (interaction.isButton() && interaction.isRepliable() && !interaction.replied && !interaction.deferred) return;
+   static async reply(interaction: any, error: Error, time: number = 2_000): Promise<void> {
+      if (interaction.isButton() && interaction.isRepliable() && interaction.replied && interaction.deferred) return;
 
       if (!interaction.isButton() && !interaction.isChatInputCommand()) return;
 
+      const controlPanelStatus = PanelStatusHandler.get(
+         interaction.client,
+         interaction.guildId!
+      )
+
+      const embedComponent = new EmbedComponent()
+         .setLang(controlPanelStatus?.getLang ?? 'es')
+
       if (error instanceof CustonError) {
          await interaction.reply({
-            embeds: [EmbedComponent.error(error.message)],
+            embeds: [embedComponent.error(error.typeMessage as any)],
          })
+
          const response = await interaction.fetchReply();
          Timeout.delete(response, time)
          return;
       }
       await interaction.reply({
-         embeds: [EmbedComponent.error('Error inesperado del servidor')],
+         embeds: [embedComponent.error('errro500')],
       })
       const response = await interaction.fetchReply();
       Timeout.delete(response, time)
    }
 
    static async editReply(interaction: CustonInteraction, error: Error, time: number = 2_000): Promise<void> {
-      if (interaction.isButton() && interaction.isRepliable() && !interaction.replied && !interaction.deferred) return;
+      
+      if (interaction.isButton() && interaction.isRepliable() && interaction.replied && interaction.deferred) return;
 
       if (!interaction.isButton() && !interaction.isChatInputCommand()) return;
 
+      const controlPanelStatus = PanelStatusHandler.get(
+         interaction.client,
+         interaction.guildId!
+      )
+      const embedComponent = new EmbedComponent()
+         .setLang(controlPanelStatus?.getLang)
+
       if (error instanceof CustonError) {
          await interaction.editReply({
-            embeds: [EmbedComponent.error(error.message)],
+            embeds: [embedComponent.error(error.message as any)],
          })
          const response = await interaction.fetchReply();
          Timeout.delete(response, time)
          return;
       }
       await interaction.editReply({
-         embeds: [EmbedComponent.error('Error inesperado del servidor')],
+         embeds: [embedComponent.error('errro500')],
       })
       const response = await interaction.fetchReply();
       Timeout.delete(response, time)
    }
 
    static async send(queue: Queue, error: Error, time: number = 2_000): Promise<void> {
+
+      const controlPanelStatus = PanelStatusHandler.get(
+         queue.client as any,
+         queue?.textChannel?.guildId!
+      )
+
+      const embedComponent = new EmbedComponent()
+         .setLang(controlPanelStatus?.getLang)
+
       if (error instanceof CustonError) {
          const response = await queue.textChannel?.send({
-            embeds: [EmbedComponent.error(error.message)],
+            embeds: [embedComponent.error(error.message as any)],
          })
          if (!response) return;
 
@@ -58,7 +84,7 @@ export class ErrorService {
          return
       }
       const response = await queue.textChannel?.send({
-         embeds: [EmbedComponent.error('Error inesperado del servidor')],
+         embeds: [embedComponent.error('errro500')],
       });
 
       if (!response) return;
