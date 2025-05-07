9560da69d1f9b657bb077a215ecdbc2e2903eba4
John <johnelvis963@gmail.com>
2025-05-06 19:46:22 -0500

    Nuevo comando de barra para el cambio de idoma

---
+++ src/interface/commands/language.ts
@@ -0,0 +1,66 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { CustonInteraction } from '../../doman/types';
+import { Timeout } from '../../utils';
+import { EmbedComponent } from '../../infrastructure/discord';
+import { ErrorService, LangService } from '../../application/service';
+import { PanelStatusHandler } from '../../application/handler/controlPanel';
+
+const languageChoices = [
+   { name: '🇪🇸 Español', value: 'es' },
+   { name: '🇬🇧 English', value: 'en' },
+   // { name: '🇫🇷 Français', value: 'fr' },
+   // { name: '🇩🇪 Deutsch', value: 'de' },
+   // { name: '🇮🇹 Italiano', value: 'it' }
+];
+const languageEmoji = {
+   es: '🇪🇸',
+   en: '🇬🇧',
+}
+
+const DEFAULT_LANG = 'es';
+
+const commandData = new SlashCommandBuilder()
+   .setName('language')
+   .setDescription('Cambia de idioma')
+   .addStringOption(option =>
+      option
+         .setName('language')
+         .setDescription('Selecciona un idioma')
+         .setRequired(true)
+         .addChoices(...languageChoices)
+   );
+
+const execute = async (interaction: CustonInteraction) => {
+   if (!interaction.isChatInputCommand()) return;
+
+   try {
+      const lang = interaction.options.getString('language') ?? DEFAULT_LANG;
+      const guildId = interaction.guildId!;
+      const client = interaction.client;
+
+      LangService.set(guildId, lang);
+
+      const controlPanelStatus = PanelStatusHandler.get(client, guildId);
+      controlPanelStatus?.setLang(lang);
+
+      const embed = new EmbedComponent()
+         .setLang(lang)
+         .settingLanguaje(languageEmoji[lang as keyof typeof languageEmoji]);
+
+      // Envía respuesta con embed
+      await interaction.reply({ embeds: [embed] });
+
+      // Elimina respuesta tras 15s
+      const response = await interaction.fetchReply();
+      Timeout.delete(response, 15_000);
+
+   } catch (error) {
+      console.error('Error en /language:', error);
+      ErrorService.editReply(interaction, error as Error);
+   }
+};
+
+export const command = {
+   data: commandData,
+   execute
+};
