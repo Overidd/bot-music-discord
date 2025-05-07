7c501d0c5ed608523ad8c331de775f7ddf6d1e3c
John <johnelvis963@gmail.com>
2025-05-06 19:39:51 -0500

    fix

--- src/application/handler/controlPanel/controlPanel.ts
+++ src/application/handler/controlPanel/controlPanel.ts
@@ -3,7 +3,8 @@ import { ControlPanelStatusEntity } from '../../../doman/entity';
 
 export class PanelStatusHandler {
    static create(client: ClientDiscord, entity: ControlPanelStatusEntity) {
-      if (entity?.guildId && client.controlPanelStatus.has(entity.guildId)) return;
+      if (entity?.guildId && client.controlPanelStatus.has(entity.guildId))
+         return;
 
       client?.controlPanelStatus.set(
          entity.guildId!,
@@ -24,11 +25,12 @@ export class PanelStatusHandler {
    static get(client: ClientDiscord, guildId: string): ControlPanelStatusEntity | undefined {
       if (!guildId) {
          // throw new Error('No implement guildId')
+         return
       }
       if (!client.controlPanelStatus.has(guildId)) {
          // throw new Error('No implement controlPanelStatus')
+         return
       }
-
       return client.controlPanelStatus.get(guildId)
    }
 }
