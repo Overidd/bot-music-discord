a2064da8dbf4b8d0bfda55bdaecb0ad0c1c0ad76
John <johnelvis963@gmail.com>
2025-05-06 19:43:38 -0500

    refactor: update CustonError class to include language support and improve validation method

--- src/doman/error/custon.ts
+++ src/doman/error/custon.ts
@@ -1,19 +1,25 @@
 
+type typeErrorValidation = 'errorNotPlaying' | 'errorNoQueueToSkip' | 'errorNoHistoryToBack' | 'errro500';
+
 export class CustonError extends Error {
-   constructor(message: string) {
-      super(message);
+   lang?: string;
+   typeMessage: string;
+   constructor(typeMessage: string, lang?: string) {
+      super(typeMessage);
+      this.lang = lang;
+      this.typeMessage = typeMessage;
    }
 
-   public static validation(message: string) {
-      return new CustonError(message);
+   public static validation(typeMessage: typeErrorValidation, lang?: string) {
+      return new CustonError(typeMessage, lang);
    }
-   public static notFound(message: string) {
-      return new CustonError(message);
+   public static notFound(typeMessage: string, lang?: string) {
+      return new CustonError(typeMessage, lang);
    }
-   public static notAllowed(message: string) {
-      return new CustonError(message);
+   public static notAllowed(typeMessage: string, lang?: string) {
+      return new CustonError(typeMessage, lang);
    }
-   public static notImplemented(message: string) {
-      return new CustonError(message);
+   public static notImplemented(typeMessage: string, lang?: string) {
+      return new CustonError(typeMessage, lang);
    }
 }
